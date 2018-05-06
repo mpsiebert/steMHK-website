2025f04cfa21fcf73fe221f6c01aee219c42acab
Haley Canfield <hcanfield@ksu-wireless-229-150.campus.ksu.edu>
2018-05-06 15:58:11 -0500

    new

--- main.js
+++ main.js
@@ -1,38 +1,11 @@
-var http = require("http")
-var fs = require("fs")
+var sqlite3 = require('sqlite3').verbose();
 
+var db = new sqlite3.Database('users.sqlite3');
 
-// utility modules in Node.js
+db.run("CREATE TABLE users (org_name TEXT, email TEXT, pass TEXT)");
 
-// used for handling & transforming file paths
-var path = require("path")
-
-// used for a few basic operating-system related utility functions
-var os = require("os")
-
-// read files
-fs.readFile('input.txt', function(err, data) {
-  if(err) return console.error(err);
-  console.log(data.toString());
-});
-
-console.log("Program Ended")
-
-// the file & directory where this file is located
-console.log( __filename )
-console.log( __dirname )
-
-// creating a basic server
-/* http.createServer(function (req, res) {
-  // send the http header
-  // http status: 200 : OK
-  // Content Type: text/plain
-
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-
-  // send the response body as "Hello World"
-  res.end('Hello World\n');
-}).listen(8081);
-
-// console will print the message
-console.log('Server running at http://127.0.0.1:8081/'); */
+for(var i = 0; i < 10; i++)
+{
+  db.run("INSERT INTO users (org_name, email, pass) VALUES (?, ?, ?)")
+}
+db.close();
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,4 +1,3 @@
-
 function formValidation()
 {
   var uemail = document.registration.email;
@@ -39,3 +38,27 @@ function passid_validation(passid,mx,my)
   }
   return true;
 }
+
+function handleRequest(req, res) {
+
+    // Check for authentication
+    var auth = req.headers.authorization;
+    if(auth) {
+        var b = new Buffer(auth.split(' ')[1], 'base64'),
+            s = b.toString(),
+            credentials = s.split(':'),
+            username = credentials[0],
+            password = credentials[1];
+
+        // TODO: check username/password pair, and if valid allow access to resource
+        console.log(username, password)
+        res.writeHead(200, {"Content-Type":"text/html"});
+        res.end("Welcome " + username + "!");
+        return;
+    }
+
+    // If no authentication was provided, serve a 401 error and request Basic Authentication
+    res.writeHead(401, {'WWW-Authenticate': 'Basic'});
+    res.end()
+}
+new require(‘http’).Server(handleRequest).listen(3000);
